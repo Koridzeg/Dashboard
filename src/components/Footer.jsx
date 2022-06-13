5c18e37d526b1937fe3fdf2a440cdc6ae86ac1cd
KoridzeG <giorgiqori@yahoo.com>
2022-06-13 18:12:41 +0400

    first

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Footer = () => (
+  <div className="mt-24">
+    <p className="dark:text-gray-200 text-gray-700 text-center m-20">
+      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Rerum
+      necessitatibus impedit veritatis qui, distinctio quo ut nulla quaerat fuga
+      inventore facere nihil obcaecati veniam, soluta autem magnam rem esse
+      tempore.
+    </p>
+  </div>
+);
+
+export default Footer;
